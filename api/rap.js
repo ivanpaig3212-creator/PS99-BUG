84bd9634747607542a98e9d1f2dd463e8c191ca3
ivanpaig3212-creator <ivanpaig3212@gmail.com>
2026-07-03 10:32:37 +0800

    Update rap.js

--- api/rap.js
+++ api/rap.js
@@ -3,12 +3,18 @@ export default async function handler(req, res) {
     const response = await fetch("https://ps99rap.com/api/items");
 
     if (!response.ok) {
-      throw new Error("Failed to fetch PS99RAP API");
+      throw new Error("Failed to fetch PS99RAP");
     }
 
     const data = await response.json();
 
-    res.status(200).json(data);
+    const items = Object.values(data);
+
+    res.status(200).json({
+      success: true,
+      updated: new Date().toISOString(),
+      items
+    });
 
   } catch (err) {
     res.status(500).json({
