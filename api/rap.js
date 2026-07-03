36ba1b20c82032bfebfa091349ab792f1de2bc96
ivanpaig3212-creator <ivanpaig3212@gmail.com>
2026-07-03 10:08:22 +0800

    Update rap.js

--- api/rap.js
+++ api/rap.js
@@ -1,16 +1,22 @@
-items: [
-  {
-    name: "Huge Happy Rock",
-    rap: 120000000,
-    previous: 100000000,
-    type: "Huge",
-    image: "https://placehold.co/300x300?text=Huge+Happy+Rock"
-  },
-  {
-    name: "Huge Cat",
-    rap: 890000000,
-    previous: 850000000,
-    type: "Huge",
-    image: "https://placehold.co/300x300?text=Huge+Cat"
-  }
-]
+export default function handler(req, res) {
+  res.status(200).json({
+    success: true,
+    updated: new Date().toISOString(),
+    items: [
+      {
+        name: "Huge Happy Rock",
+        rap: 120000000,
+        previous: 100000000,
+        type: "Huge",
+        image: "https://placehold.co/300x300?text=Huge+Happy+Rock"
+      },
+      {
+        name: "Huge Cat",
+        rap: 890000000,
+        previous: 850000000,
+        type: "Huge",
+        image: "https://placehold.co/300x300?text=Huge+Cat"
+      }
+    ]
+  });
+}
